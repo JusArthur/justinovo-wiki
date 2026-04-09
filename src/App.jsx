65eafc0fbc77edb4b31aafe81d37d6b1fa9ff4c1
JusArthur <arthurkingjustin@gmail.com>
2026-04-08 21:55:19 -0500

    Refactor song management in App component: Remove hardcoded song data and import songs from a utility file for improved maintainability and scalability.

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,10 @@ import RealClock from './components/RealClock';
 import RealCalendar from './components/RealCalendar';
 import GreetingBox from './components/GreetingBox';
 import StarshipGameBackground from './components/StarshipGameBackground.jsx';
+import songs from './utils/songs.jsx'
 
 function App() {
-  const songs = [
-    {
-      id: 'next-to-you',
-      artist: 'parasyte',
-      fileName: 'next_to_you.flac',
-      title: {EN: 'Next To You', CN: 'Next To You'}
-    },
-    {
-      id: 'jaychou-i-do',
-      artist: 'Jay Chou',
-      fileName: '周杰伦 - I Do.flac',
-      title: { EN: 'I Do', CN: 'I Do' },
-    },
-    {
-      id: 'jaychou-xiang-nv-duo-qing',
-      artist: 'Jay Chou',
-      fileName: '周杰伦 - 湘女多情.flac',
-      title: { EN: 'The girl from Hunan', CN: '湘女多情' },
-    },
-    {
-      id: 'jaychou-aegean-sea',
-      artist: 'Jay Chou',
-      fileName: '周杰伦 - 爱琴海.flac',
-      title: { EN: 'Aegean Sea', CN: '爱琴海' },
-    },
-  ];
+
 
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
---
+++ src/utils/songs.jsx
@@ -0,0 +1,28 @@
+const songs = [
+    {
+      id: 'next-to-you',
+      artist: 'parasyte',
+      fileName: 'next_to_you.flac',
+      title: {EN: 'Next To You', CN: 'Next To You'}
+    },
+    {
+      id: 'jaychou-i-do',
+      artist: 'Jay Chou',
+      fileName: '周杰伦 - I Do.flac',
+      title: { EN: 'I Do', CN: 'I Do' },
+    },
+    {
+      id: 'jaychou-xiang-nv-duo-qing',
+      artist: 'Jay Chou',
+      fileName: '周杰伦 - 湘女多情.flac',
+      title: { EN: 'The girl from Hunan', CN: '湘女多情' },
+    },
+    {
+      id: 'jaychou-aegean-sea',
+      artist: 'Jay Chou',
+      fileName: '周杰伦 - 爱琴海.flac',
+      title: { EN: 'Aegean Sea', CN: '爱琴海' },
+    },
+  ];
+
+export default songs;
